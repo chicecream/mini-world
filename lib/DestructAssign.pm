458e405c5066fda06e1bac6124bb55733211cf7c
chicecream <108047943+chicecream@users.noreply.github.com>
2022-07-26 20:21:33 +0000

    Update members.yaml

---
+++ lib/DestructAssign.pm
@@ -0,0 +1,29 @@
+
+package DestructAssign;
+
+use 5.008009;
+use strict;
+use warnings;
+use Carp;
+
+require Exporter;
+use AutoLoader;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use DestructAssign ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+    des des_alias
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
