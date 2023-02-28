0b5381049856936d78816faa73585d9be7f09415
chicecream <108047943+chicecream@users.noreply.github.com>
2023-02-28 14:28:56 +0000

    Updates Podfile.lock

--- try.pl
+++ try.pl
@@ -44,3 +44,61 @@ sub tt($) : lvalue {}
 #    # $d = undef
 # }
 # 順便幫忙 my
+# {
+#    des_my [$a, {b => $b, c => [undef, $c]] = [1, {a => 'a', b => [1,2,3], c => [5, 6]];
+#    # 得到 (而且都自動加上 lexical 宣告)
+#    # $a = 1
+#    # $b = [1,2,3]
+#    # $c = 6
+# }
+# alias 型式
+# {
+#    my $data = [1, {a => 'a', b => [1,2,3], c => [5, 6]];
+#
+#    des_my_alias [$a, {b => $b, c => [undef, $c]] = $data;
+#    # or
+#    my ($a, $b, $c);
+#    des_alias [$a, {b => $b, c => [undef, $c]] = $data;
+#
+#    $a = 2; $b = { b => 'b' }; $c = 'oo';
+#    # 得到
+#    # $data = [2, {a => 'a', b => {b => 'b'}, c => [5, 'oo']];
+# }
+
+sub f {
+    my($q, $g, @remain, %hv, $o);
+    des [my $x, $y, my $z] = \@_;
+    print "\$x=$x \$y=$y \$z=$z\n";
+    des [undef, $x, -2 => $z, [$y]] = [3, 4, 5, [6], 8, [7]];
+    print "\$x=$x \$y=$y \$z=$z\n";
+    des {x => $x, y => $y} = {x => 'x', y => 'y'};
+    print "\$x=$x \$y=$y \$z=$z\n";
+    @remain = ('a'..'c');
+    des [$x, {oo => [$A::o]}, -5 => @remain, -5 => %hv, -5 => @A::array, -4 => %A::hash] = ['x', {oo => ['Ao']}, 1..9];
+    print "\$x=$x \$A::o=$A::o \@remain=@remain \@A::array=@A::array\n";
+    local $Data::Dumper::Indent = 0;
+    print '%hv=', Dumper(\%hv), $/;
+    print '%A::hash=', Dumper(\%A::hash), $/;
+    #des [ *main::yy ] = [sub { print "yy\n" }];
+
+    #yy();
+}
+
+BEGIN {
+    my $depth = 0;
+    my $silent = 0;
+    for my $entry ( keys %B::Deparse:: ) {
+        no strict 'refs';
+        if( my $f = *{"B::Deparse\::$entry"}{CODE} ) {
+            no warnings 'redefine';
+            no warnings 'prototype';
+            *{"B::Deparse\::$entry"} = sub {
+                if( $silent ) {
+                    return &$f;
+                }
+                if( grep { $entry eq $_ } qw(pessimise class null is_state is_scope is_miniwhile is_for_loop) ) {
+                    my $ori_silent = $silent;
+                    $silent = 1;
+                    if( wantarray ) {
+                        my @res = &$f;
+                        $silent = $ori_silent;
